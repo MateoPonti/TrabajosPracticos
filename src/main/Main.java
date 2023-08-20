47fafc08b0b823e9701271ff45fe4de5f3451a62
MateoPonti <63692136+MateoPonti@users.noreply.github.com>
2023-08-19 23:14:04 -0300

    cambios en main y lista

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,5 @@
 package main;
+import unlu.poo.lista.simple.Lista;
 import unlu.poo.pila.Pila;
 
 import java.util.Locale;
@@ -13,24 +14,27 @@ public class Main {
 
     }
 
+    private static void dejarEspacio()  {
+        System.out.println("     ");
+        System.out.println("     ");
+        System.out.println("     ");
+    }
+
+
     private static void arrancarBucle(){
         Scanner sc = new Scanner(System.in);
         boolean bucle=true;
 
         while (bucle){
             mostrarMenu();
-            String option= sc.nextLine().toUpperCase(Locale.ROOT);
+            String option= sc.nextLine();
             switch (option) {
                 case ("0") -> bucle = false;
                 case ("1") -> mostrarEjercicio1();
                 case ("3") -> mostrarEjercicio3();
                 default -> System.out.println("No existe esa Opcion.");
             }
-
-            System.out.println("   ");
-            System.out.println("   ");
-            System.out.println("   ");
-
+            dejarEspacio();
     }
     }
     private static void mostrarMenu(){
@@ -38,43 +42,55 @@ public class Main {
         System.out.println("-ingrese 0 para salir");
     }
     private static void mostrarEjercicio1(){
+        Lista lista= new Lista();
+
 
 
     }
     private static void mostrarEjercicio3(){
         Pila pila= new Pila();
-        System.out.println("Ejercicio 3:");
-        pila.apilar("Hola");
-        pila.apilar("Mundo");
-        pila.apilar("!");
-
-        pila.desapilar();
-
-        System.out.println(pila.getTope());
-        // Espero "Mundo"
-
-        pila.desapilar();
-        System.out.println(pila.getTope());
-        // Espero "Hola"
-
-        pila.desapilar();
-        System.out.println(pila.getTope());
-        // Espero "Pila Vacia"
-
-
-        pila.apilar("a");
-        System.out.println(pila.getTope());
-        // Espero "a"
-        pila.apilar(3);
-        System.out.println(pila.getTope());
-        //Espero "3"
-
-        pila.desapilar();
-        pila.desapilar();
-        pila.desapilar();
-        pila.desapilar();
-        // Espero que este vacia y que no haya producido algun error.
-        System.out.println(pila.getTope());
-
+        boolean salir= false;
+        Scanner sc = new Scanner(System.in);
+        while (!salir){
+            System.out.println("0 para salir");
+            System.out.println("1-Apilar Dato");
+            System.out.println("2-Desapilar Dato");
+            System.out.println("3-Mostrar Tope");
+            System.out.println("4-Mostrar si esta Vacia");
+            String option= sc.nextLine();
+            switch (option) {
+                case ("0") -> salir = true;
+                case ("1") -> {
+                    System.out.println("ingrese palabra:");
+                    String palabra = sc.nextLine();
+                    pila.apilar(palabra);
+                }
+                case ("2") -> {
+                    if (pila.estaVacia()) {
+                        System.out.println("La pila ya se encuentra vacia.");
+                    } else {
+                        pila.desapilar();
+                        System.out.println("Desapilado correctamente");
+                    }
+                }
+                case ("3") -> System.out.println(pila.getTope());
+                case ("4") -> {
+                    if (pila.estaVacia()) {
+                        System.out.println("La pila se encuentra Vacia");
+                    } else {
+                        System.out.println("La pila no esta Vacia.");
+                    }
+                }
+            }
+            dejarEspacio();
     }
+   pila=null;// la dejo de apuntar asi deja de estar en memoria
+
+}
+
+
+
+
+
+
 }
--- src/unlu/poo/lista/simple/Lista.java
+++ src/unlu/poo/lista/simple/Lista.java
@@ -1,13 +1,40 @@
 package unlu.poo.lista.simple;
 
+import unlu.poo.nodo.Nodo;
+
 public class Lista {
     private Nodo proximo;
+    private int items;
+
+    public void agregar(Object valor){
+        Nodo nodoAux= proximo;
+        while (!(nodoAux==null)){
+            nodoAux=nodoAux.getProximo();
+        }
+        nodoAux.setDato(proximo);
+        items++;
+    }
 
 
-    public static void agregar(){}
+    public void eliminar(int posicion){
+        if (proximo!=null){
+            posicion--;
+            if (posicion==0){proximo=proximo.getProximo();
+                items--;}
+            else {
+            int contador=0;
+            Nodo nodoAux= proximo;
+            while((contador<posicion-1) && (nodoAux!=null)){nodoAux=nodoAux.getProximo();}
+            if (contador==posicion-1){
+             nodoAux.setProximo(nodoAux.getProximo().getProximo());
+             contador++;
+             items--;
+              }
+            }
+        }
+    }
 
-
-    public static void eliminar(int posicion){
-
+    public int getItems() {
+        return items;
     }
 }
