4069e9e0e440d135a1bac9b9f2568906f3c50294
MateoPonti <63692136+MateoPonti@users.noreply.github.com>
2023-08-20 23:52:22 -0300

    cambios en main y lista

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,9 +14,7 @@ public class Main {
     }
 
     private static void dejarEspacio()  {
-        System.out.println("     ");
-        System.out.println("     ");
-        System.out.println("     ");
+        System.out.println("");
     }
 
 
@@ -37,14 +35,88 @@ public class Main {
     }
     }
     private static void mostrarMenu(){
-        System.out.println("-ingrese el numero segun que ejercicio quiere (1-17)");
+        System.out.println("-ingrese el numero segun que ejercicio quiere (1-13)");
         System.out.println("-ingrese 0 para salir");
     }
     private static void mostrarEjercicio1(){
         Lista lista= new Lista();
-
-
-
+        boolean salir= false;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingresar posiciones ordinales, arranca desde el 1");
+        while (!salir){
+            System.out.println("0 para salir");
+            System.out.println("1-Agregar Dato a la Lista");
+            System.out.println("2-Eliminar Dato");
+            System.out.println("3-Insertar Dato");
+            System.out.println("4-Mostrar Cantidad de Elementos");
+            System.out.println("5-Mostrar Elemento");
+            System.out.println("6-Mostrar si esta Vacia");
+            System.out.println("7-Mostrar Lista");
+            String option= sc.nextLine();
+            switch (option) {
+                case ("0") -> salir = true;
+                case ("1") -> {
+                    System.out.println("ingrese palabra:");
+                    String palabra = sc.nextLine();
+                    lista.agregar(palabra);
+                    System.out.println("Dato agregado");
+                }
+                case ("2") -> {
+                    if (lista.estaVacia()) {
+                        System.out.println("La Lista ya se encuentra vacia.");
+                    } else {
+                        System.out.println("ingrese posicion a eliminar:");
+                        int posicion = sc.nextInt();
+                        if (lista.eliminar(posicion)){
+                            System.out.println("Eliminado correctamente");
+                        }
+                        else{
+                            System.out.println("No se pudo eliminar, fijese que sea una posicion valida o la lista no este vacia.");
+                        };
+                    }
+                }
+                case ("3") -> {
+                    System.out.println("ingrese palabra:");
+                    String palabra = sc.nextLine();
+                    System.out.println("ingrese posicion:");
+                    int posicion= sc.nextInt();
+                    if (lista.insertar(posicion,palabra)){
+                        System.out.println(palabra + " insertado Correctamente.");
+                    }
+                    else {
+                        System.out.println("No se pudo insertar, fijese que sea una posicion valida o la lista no este vacia.");
+                    };
+                    System.out.println();
+                }
+                case ("4") -> {
+                    System.out.println("La cantidad de elementos de la lista es : "+lista.getItems());
+                }
+                case ("5") -> {
+                    System.out.println("ingrese posicion:");
+                    int posicion= sc.nextInt();
+                    Object dato=lista.getElemento(posicion);
+                    if (dato!=null){
+                        System.out.println("El elemento de la posicion "+posicion+" es "+dato.toString());
+                    }
+                    else {
+                        System.out.println("Elemento nulo o posicion invalida.");
+                    }
+                }
+                case ("6") -> {
+                    if (lista.estaVacia()) {
+                        System.out.println("La lista se encuentra Vacia");
+                    } else {
+                        System.out.println("La lista no esta Vacia.");
+                    }
+                }
+                case ("7") -> {
+                    System.out.println("La lista es:");
+                    System.out.println(lista.toString());
+                }
+            }
+            dejarEspacio();
+        }
+        lista=null;// la dejo de apuntar asi deja de estar en memoria
     }
     private static void mostrarEjercicio3(){
         Pila pila= new Pila();
@@ -85,10 +157,4 @@ public class Main {
     }
    pila=null;// la dejo de apuntar asi deja de estar en memoria
 }
-
-
-
-
-
-
 }
--- src/unlu/poo/lista/simple/Lista.java
+++ src/unlu/poo/lista/simple/Lista.java
@@ -7,33 +7,41 @@ public class Lista {
     private int items;
 
     public void agregar(Object valor){
+        Nodo nodoNuevo= new Nodo();
+        nodoNuevo.setDato(valor);
+        if (inicio==null){
+            inicio=nodoNuevo;
+            items++;}
+        else {
         Nodo nodoAux= inicio;
         while (nodoAux.getProximo()!=null){
             nodoAux=nodoAux.getProximo();
         }
-        Nodo nodoNuevo= new Nodo();
-        nodoNuevo.setDato(valor);
         nodoAux.setProximo(nodoNuevo);
-        items++;
+        items++;}
     }
 
 
-    public void eliminar(int posicion){
-        if (inicio!=null){
+    public boolean eliminar(int posicion){
+        if ((inicio!=null)&&( (posicion>=1)&&(posicion<=items)) ){
             posicion--;
-            if (posicion==0){inicio=inicio.getProximo();
-                items--;}
+            if (posicion==0){
+                inicio=inicio.getProximo();
+                items--;
+            }
             else {
             int contador=0;
             Nodo nodoAux= inicio;
-            while((contador<posicion-1) && (nodoAux!=null)){nodoAux=nodoAux.getProximo();}
-            if ((contador==posicion-1) && (nodoAux!=null)){
-              if (nodoAux.getProximo()!=null){nodoAux.setProximo(nodoAux.getProximo().getProximo());}
-              contador++;
-              items--;
-              }
+            while(contador<posicion-1){
+                nodoAux=nodoAux.getProximo();
+                contador++;}
+            if (nodoAux.getProximo().getProximo()!=null){nodoAux.setProximo(nodoAux.getProximo().getProximo());}
+            else{nodoAux.setProximo(null);}
+            items--;
             }
+            return  true;
         }
+        return false;
     }
 
     public int getItems() {
@@ -44,13 +52,58 @@ public class Lista {
         return inicio==null; // se puede hacer tambien comparando a items con 0
     }
 
-    public Object getElemento(int Posicion){
-
-
+    public Object getElemento(int posicion){
+        if ((inicio!=null)&&( (posicion>=1)&&(posicion<=items)) ){
+            if (posicion==0){return  inicio.getDato();}
+            else {
+                int contador=0;
+                Nodo nodoAux= inicio;
+                while(contador<posicion-1){
+                    nodoAux=nodoAux.getProximo();
+                    contador++;}
+                return  nodoAux.getDato();
+            }
+        }
+        return null;
     }
 
-    public void insertar(int posicion){
+    public boolean insertar(int posicion, Object dato){
+        if ((posicion>=1)&&(posicion<=items+1)){
+                items++;
+                if (inicio==null){
+                    agregar(dato);
+                    items--;
+                }
+                else {
+                Nodo nodoNuevo= new Nodo();
+                nodoNuevo.setDato(dato);
+                if (posicion==1)
+                {
+                 nodoNuevo.setProximo(inicio);
+                 inicio=nodoNuevo;
+                }
+                else {
+                int contador=0;
+                Nodo nodoAux= inicio;
+                while(contador<posicion-2){
+                    nodoAux=nodoAux.getProximo();
+                    contador++;}
+                nodoNuevo.setProximo(nodoAux.getProximo());
+                nodoAux.setProximo(nodoNuevo);
+                 }
+                }
+            return  true;
+            }
+        return false;
+    }
 
-
+    public String toString(){
+        Nodo nodoAux= inicio;
+        String val="";
+        while (nodoAux!=null){
+            val=val+nodoAux.getDato().toString()+"/n";
+            nodoAux=nodoAux.getProximo();
+        }
+        return  val;
     }
 }
