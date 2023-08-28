7c13aaa45e58ef97532264c7b0d5092d92701120
MateoPonti <63692136+MateoPonti@users.noreply.github.com>
2023-08-28 18:32:27 -0300

    Ejercicio 6 Libro

--- src/unlu/poo/administradorEjercicios/Ejercicio6.java
+++ src/unlu/poo/administradorEjercicios/Ejercicio6.java
@@ -5,9 +5,39 @@ import unlu.poo.libro.Libro;
 public class Ejercicio6 {
 
     public Ejercicio6() {
-        Libro libro= new Libro();
+
+        Libro libro= new Libro("Juan Enrique", "Remolino", 162,10,0);
+        Libro libro2= new Libro("Maria Posiert", "Mi planta de naranja lima", 232,1,0,"13233311");
 
 
+
+        System.out.println(libro);
+        System.out.println();
+        System.out.println();
+        System.out.println(libro2);
+        System.out.println();
+        System.out.println();
+        if (libro.getNumPaginas()>libro2.getNumPaginas()){
+            System.out.println("El libro "+libro.getTitulo()+" tiene mas paginas que el libro "+libro2.getTitulo());
+        }
+        else {
+            System.out.println("El libro "+libro2.getTitulo()+" tiene mas paginas que el libro "+libro.getTitulo());
+        }
+
+        System.out.println();
+        System.out.println("Prestamos el libro 1 que tiene 10 ejemplares disponibles");
+        libro.prestarLibro();
+        System.out.println();
+        System.out.println("El total de ejemplares disponibles del libro 1 es : "+ libro.getNumEjemplares()+" y el prestado es: "+libro.getNumEjemplaresPrestados());
+        System.out.println();
+        libro2.prestarLibro();
+        System.out.println("El total de ejemplares disponibles del libro 2 es : "+ libro2.getNumEjemplares()+" y el prestado es: "+libro2.getNumEjemplaresPrestados());
+        System.out.println();
+        System.out.println("Se ve que en el segundo caso al solo tener 1 libro disponible no lo presta");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("Total de libros prestados: "+libro.getNumEjemplaresPrestadosTotal());
     }
 
 }
--- src/unlu/poo/libro/Libro.java
+++ src/unlu/poo/libro/Libro.java
@@ -1,4 +1,100 @@
 package unlu.poo.libro;
 
 public class Libro {
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private int numPaginas;
+    private int numEjemplares;
+    private int numEjemplaresPrestados;
+    static private int numEjemplaresPrestadosTotal;
+
+
+
+    public Libro(String autor, String titulo,int numPaginas,int numEjemplares, int numEjemplaresPrestados ){
+        setAutor(autor);
+        setTitulo(titulo);
+        setNumPaginas(numPaginas);
+        setNumEjemplares(numEjemplares);
+        setNumEjemplaresPrestados(numEjemplaresPrestados);
+        this.isbn=null;
+    }
+    public Libro(String autor, String titulo,int numPaginas,int numEjemplares, int numEjemplaresPrestados,String isbn ){
+        this(autor,titulo,numPaginas,numEjemplares,numEjemplaresPrestados);
+        setIsbn(isbn);
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(int numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    public int getNumEjemplares() {
+        return numEjemplares;
+    }
+
+    public void setNumEjemplares(int numEjemplares) {
+        this.numEjemplares=1;
+        if (numEjemplares>0){
+            this.numEjemplares = numEjemplares;
+        }
+    }
+
+    public int getNumEjemplaresPrestados() {
+        return numEjemplaresPrestados;
+    }
+
+    private void setNumEjemplaresPrestados(int numEjemplaresPrestados) {
+        this.numEjemplaresPrestados=0;
+        if (numEjemplaresPrestados>0){
+        this.numEjemplaresPrestados = numEjemplaresPrestados;
+        numEjemplaresPrestadosTotal+=numEjemplaresPrestados;
+        }
+    }
+
+    public int getNumEjemplaresPrestadosTotal() {
+        return numEjemplaresPrestadosTotal;
+    }
+
+
+    public void prestarLibro(){
+        if (numEjemplares>1){
+            numEjemplaresPrestados++;
+            numEjemplaresPrestadosTotal++;
+            numEjemplares--;
+        }
+    }
+
+    public String toString() {
+        return  "El Libro"+titulo+" creado por el autor "+autor+ " tiene "+numPaginas+" paginas, quedan "+numEjemplares+" disponibles y se prestaron "+numEjemplaresPrestados+".";
+
+
+    }
 }
