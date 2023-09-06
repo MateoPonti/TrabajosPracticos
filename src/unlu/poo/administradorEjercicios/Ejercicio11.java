51f15d5249d2cc6757b5cf1f42b4d88ac588f2bb
MateoPonti <63692136+MateoPonti@users.noreply.github.com>
2023-09-06 01:16:31 -0300

    Cambios img y Juego ej 11

--- src/unlu/poo/administradorEjercicios/Ejercicio11.java
+++ src/unlu/poo/administradorEjercicios/Ejercicio11.java
@@ -5,7 +5,26 @@ import unlu.poo.juego.Juego;
 public class Ejercicio11 {
 
     public Ejercicio11(){
-        Juego juegoAMedias= new Juego();
+
+        String[] palabras = {
+                "casa", "perro", "gato", "rojo", "verde", "azul", "sol", "luna", "montaña", "playa",
+                "coche", "bicicleta", "manzana", "banana", "naranja", "libro", "computadora", "televisión", "música", "película",
+                "amigo", "familia", "amor", "alegría", "tristeza", "felicidad", "cielo", "tierra", "agua", "fuego",
+                "trabajo", "estudio", "vacaciones", "dinero", "comida", "bebida", "aire", "aire libre", "naturaleza", "ciudad",
+                "arte", "cultura", "deporte", "juego", "avión", "tren", "autobús", "metro", "barco", "trenza",
+                "guitarra", "fotografía", "reloj", "espejo", "flores", "bosque", "mariposa", "playa", "isla", "cascada",
+                "desierto", "sombrero", "lápiz", "papel", "pintura", "cine", "teatro", "escenario", "risa", "sonrisa",
+                "invierno", "verano", "primavera", "otoño", "lluvia", "nieve", "viento", "relámpago", "rayo", "tormenta",
+                "fruta", "verdura", "carne", "pescado", "arroz", "pan", "leche", "queso", "helado", "pastel",
+                "cuchillo", "tenedor", "cuchara", "plato", "vaso", "copa", "silla", "mesa", "sofá", "cama",
+                "ventana", "puerta", "techo", "piso", "escalera", "espejo", "telefono", "computadora", "ratón", "teclado",
+                "pantalla", "internet", "correo", "correo electrónico", "chat", "red social", "amistad", "amigo", "amiga", "enemigo",
+                "hermano", "hermana", "padre", "madre", "abuelo", "abuela", "niño", "niña", "joven", "adulto",
+                "viejo", "muerte", "vida", "sueño", "pesadilla", "canción", "baile", "pintura", "escultura", "dibujo", "arte",
+                "historia", "geografía", "matemáticas", "ciencia", "física", "química", "biología", "astronomía", "planeta", "estrella",
+                "luz", "color", "forma", "número", "letra", "palabra", "frase", "oración", "párrafo", "texto","kiwi", "zapato", "xilófono", "yogur", "walrus", "queso", "quesadilla", "koala", "zebra"
+        };
+        Juego juegoAMedias= new Juego(palabras);
 
 
 
--- src/unlu/poo/juego/Juego.java
+++ src/unlu/poo/juego/Juego.java
@@ -10,14 +10,18 @@ public class Juego {
     private Diccionario diccionario;
 
 
-    public Juego(){
+
+
+    public Juego(String[]  palabras){
         jugadores= new ArrayList<>();
         jugadores.add(new Jugador());
         jugadores.add(new Jugador());
 
         diccionario=new Diccionario();
 
-        inicializarDiccionario();
+        inicializarDiccionario(palabras);
+
+
     }
 
 
@@ -40,25 +44,8 @@ public class Juego {
     }
 
 
-    private void inicializarDiccionario(){
-        String[] palabras = {
-                "casa", "perro", "gato", "rojo", "verde", "azul", "sol", "luna", "montaña", "playa",
-                "coche", "bicicleta", "manzana", "banana", "naranja", "libro", "computadora", "televisión", "música", "película",
-                "amigo", "familia", "amor", "alegría", "tristeza", "felicidad", "cielo", "tierra", "agua", "fuego",
-                "trabajo", "estudio", "vacaciones", "dinero", "comida", "bebida", "aire", "aire libre", "naturaleza", "ciudad",
-                "arte", "cultura", "deporte", "juego", "avión", "tren", "autobús", "metro", "barco", "trenza",
-                "guitarra", "fotografía", "reloj", "espejo", "flores", "bosque", "mariposa", "playa", "isla", "cascada",
-                "desierto", "sombrero", "lápiz", "papel", "pintura", "cine", "teatro", "escenario", "risa", "sonrisa",
-                "invierno", "verano", "primavera", "otoño", "lluvia", "nieve", "viento", "relámpago", "rayo", "tormenta",
-                "fruta", "verdura", "carne", "pescado", "arroz", "pan", "leche", "queso", "helado", "pastel",
-                "cuchillo", "tenedor", "cuchara", "plato", "vaso", "copa", "silla", "mesa", "sofá", "cama",
-                "ventana", "puerta", "techo", "piso", "escalera", "espejo", "telefono", "computadora", "ratón", "teclado",
-                "pantalla", "internet", "correo", "correo electrónico", "chat", "red social", "amistad", "amigo", "amiga", "enemigo",
-                "hermano", "hermana", "padre", "madre", "abuelo", "abuela", "niño", "niña", "joven", "adulto",
-                "viejo", "muerte", "vida", "sueño", "pesadilla", "canción", "baile", "pintura", "escultura", "dibujo", "arte",
-                "historia", "geografía", "matemáticas", "ciencia", "física", "química", "biología", "astronomía", "planeta", "estrella",
-                "luz", "color", "forma", "número", "letra", "palabra", "frase", "oración", "párrafo", "texto","kiwi", "zapato", "xilófono", "yogur", "walrus", "queso", "quesadilla", "koala", "zebra"
-        };
+    private void inicializarDiccionario(String[] palabras){
+
 
         for (String palabra : palabras) {
             diccionario.agregarPalabra(palabra);
